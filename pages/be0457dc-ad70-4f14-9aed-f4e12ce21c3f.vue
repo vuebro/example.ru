11394c21a7d8df7bb583bde8262c0e26a6a9f75e
Jerry Bruwes <jbruwes@icloud.com>
2025-05-05 14:00:13 +0200

    2025-05-05

--- pages/be0457dc-ad70-4f14-9aed-f4e12ce21c3f.vue
+++ pages/be0457dc-ad70-4f14-9aed-f4e12ce21c3f.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-dvh" un-cloak>
     <el-page-header :icon="AlarmClock" :content="pages[$route.name].title" :title="title" @back="$router.push('/')"
-      class="sticky top-0 z-50 pa-4 border-b bg-neutral-50"></el-page-header>
+      class="sticky top-0 z-50 pa-4 border-b border-slate-200 bg-neutral-50"></el-page-header>
     <router-view></router-view>
   </div>
   <div class="flex flex-col gap-8 items-center bg-neutral-200 pa-12 not-prose" un-cloak>
@@ -12,12 +12,15 @@
   </div>
 </template>
 <script setup>
-import "https://cdn.jsdelivr.net/npm/element-plus@^2/dist/index.css";
 import ElementPlus from "element-plus";
 import { AlarmClock } from "@element-plus/icons-vue";
-import { inject } from "vue";
-window.app.use(ElementPlus);
-const { id } = defineProps(["id"]),
+import { inject, getCurrentInstance } from "vue";
+const { appContext: { app } } = getCurrentInstance(),
+  { id } = defineProps(["id"]),
   pages = inject("pages"),
   { title, description, $children } = pages[id];
+app.use(ElementPlus);
 </script>
+<style>
+@import "https://cdn.jsdelivr.net/npm/element-plus@^2/dist/index.css";
+</style>
